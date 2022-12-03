49fe6249cf23950e25a45ed475e10ba3fd2ed790
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2022-12-03 16:45:52 -0600

    Add all files for day 02

---
+++ day02/part1.py
@@ -0,0 +1,33 @@
+ROCK, PAPER, SCISSORS = "A", "B", "C"
+
+# strategy translation
+strategy = {"X": ROCK, "Y": PAPER, "Z": SCISSORS}
+
+play_value = {ROCK: 1, PAPER: 2, SCISSORS: 3}
+
+match_result_value = {
+    ROCK: {ROCK: 3, PAPER: 6, SCISSORS: 0},
+    PAPER: {ROCK: 0, PAPER: 3, SCISSORS: 6},
+    SCISSORS: {ROCK: 6, PAPER: 0, SCISSORS: 3},
+}
+
+
+def solution(filename: str) -> int:
+    with open(filename, "r") as fp:
+        plays: str = fp.read().splitlines()
+
+    points: int = 0
+    for play in plays:
+        other_play, my_strategy = play.split()
+        my_play = strategy[my_strategy]
+        points += play_value[my_play] + match_result_value[other_play][my_play]
+
+    return points
+
+
+if __name__ == "__main__":
+    result: int = solution("./example.txt")
+    print(result)  # it should be 12
+
+    result: int = solution("./input.txt")
+    print(result)
---
+++ day02/part2.py
@@ -0,0 +1,38 @@
+ROCK, PAPER, SCISSORS = "A", "B", "C"
+
+# strategy translation
+LOSE, DRAW, WIN = "X", "Y", "Z"
+choice = {
+    LOSE: {ROCK: SCISSORS, PAPER: ROCK, SCISSORS: PAPER},
+    DRAW: {ROCK: ROCK, PAPER: PAPER, SCISSORS: SCISSORS},
+    WIN: {ROCK: PAPER, PAPER: SCISSORS, SCISSORS: ROCK},
+}
+
+play_value = {ROCK: 1, PAPER: 2, SCISSORS: 3}
+
+match_result_value = {
+    ROCK: {ROCK: 3, PAPER: 6, SCISSORS: 0},
+    PAPER: {ROCK: 0, PAPER: 3, SCISSORS: 6},
+    SCISSORS: {ROCK: 6, PAPER: 0, SCISSORS: 3},
+}
+
+
+def solution(filename: str) -> int:
+    with open(filename, "r") as fp:
+        plays: str = fp.read().splitlines()
+
+    points: int = 0
+    for play in plays:
+        other_play, my_strategy = play.split()
+        my_play = choice[my_strategy][other_play]
+        points += play_value[my_play] + match_result_value[other_play][my_play]
+
+    return points
+
+
+if __name__ == "__main__":
+    result: int = solution("./example.txt")
+    print(result)  # it should be 15
+
+    result: int = solution("./input.txt")
+    print(result)

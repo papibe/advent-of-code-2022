1f6de5b6a5fb1ea25be0e6e7888c96636374db67
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2022-12-03 15:28:14 -0600

    Add day 1 solutions and extra files

---
+++ day01/part1.py
@@ -0,0 +1,20 @@
+def solution(filename: str) -> int:
+    with open(filename, "r") as fp:
+        raw_data: str = fp.read()
+
+    max_calories: int = 0
+    for elf_snack_calories in raw_data.split("\n\n"):
+        total_elf_calories: int = sum(
+            [int(calorie) for calorie in elf_snack_calories.splitlines()]
+        )
+        max_calories = max(max_calories, total_elf_calories)
+
+    return max_calories
+
+
+if __name__ == "__main__":
+    result: int = solution("./example.txt")
+    print(result)  # it should be 24000
+
+    result: int = solution("./input.txt")
+    print(result)
---
+++ day01/part2.py
@@ -0,0 +1,34 @@
+def solution(filename: str) -> int:
+    with open(filename, "r") as fp:
+        raw_data: str = fp.read()
+
+    # set initial values for 3 maxs
+    max_calories_1: int = 0
+    max_calories_2: int = 0
+    max_calories_3: int = 0
+
+    for elf_snack_calories in raw_data.split("\n\n"):
+        total_elf_calories: int = sum(
+            [int(calorie) for calorie in elf_snack_calories.splitlines()]
+        )
+
+        # update max values if necessary
+        if total_elf_calories > max_calories_1:
+            max_calories_3 = max_calories_2
+            max_calories_2 = max_calories_1
+            max_calories_1 = total_elf_calories
+        elif total_elf_calories > max_calories_2:
+            max_calories_3 = max_calories_2
+            max_calories_2 = total_elf_calories
+        elif total_elf_calories > max_calories_3:
+            max_calories_3 = total_elf_calories
+
+    return max_calories_1 + max_calories_2 + max_calories_3
+
+
+if __name__ == "__main__":
+    result: int = solution("./example.txt")
+    print(result)  # it should be 24000
+
+    result: int = solution("./input.txt")
+    print(result)

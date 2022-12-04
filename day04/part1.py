a3f60a190fc98dfe04ef02a9f46bf7f8b0ec3d64
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2022-12-04 16:47:05 -0600

    Add solutions and files for day 4

---
+++ day04/part1.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def is_contained(fi: str, fe: str, si: str, se: str) -> bool:
+    return (int(fi) <= int(si) and int(se) <= int(fe)) or (
+        int(si) <= int(fi) and int(fe) <= int(se)
+    )
+
+
+def solution(filename: str) -> int:
+    with open(filename, "r") as fp:
+        section_pairs: List = fp.read().splitlines()
+
+    fully_contained: int = 0
+    for pair in section_pairs:
+        first_pair, second_pair = pair.split(",")
+        first_init, first_end = first_pair.split("-")
+        second_init, second_end = second_pair.split("-")
+
+        if is_contained(first_init, first_end, second_init, second_end):
+            fully_contained += 1
+
+    return fully_contained
+
+
+if __name__ == "__main__":
+    result: int = solution("./example.txt")
+    print(result)  # it should be 2
+
+    result: int = solution("./input.txt")
+    print(result)
---
+++ day04/part2.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+def overlaps(fi: str, fe: str, si: str, se: str) -> bool:
+    if fe < si or se < fi:
+        return False
+    else:
+        return True
+
+
+def solution(filename: str) -> int:
+    with open(filename, "r") as fp:
+        section_pairs: List = fp.read().splitlines()
+
+    total_overlaps: int = 0
+    for pair in section_pairs:
+        first_pair, second_pair = pair.split(",")
+        first_init, first_end = first_pair.split("-")
+        second_init, second_end = second_pair.split("-")
+
+        if overlaps(int(first_init), int(first_end), int(second_init), int(second_end)):
+            total_overlaps += 1
+
+    return total_overlaps
+
+
+if __name__ == "__main__":
+    result: int = solution("./example.txt")
+    print(result)  # it should be 4
+
+    result: int = solution("./input.txt")
+    print(result)

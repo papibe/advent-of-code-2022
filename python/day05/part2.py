63c906cb1d9a714a8eb32f9d00c370156c6ecf0b
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2026-07-08 14:43:36 -0500

    Refactor and lint day 5 in python. Both parts

--- python/day05/part2.py
+++ python/day05/part2.py
@@ -1,40 +1,67 @@
 import re
-from typing import List
+from collections import namedtuple
+from typing import Dict, List, Match, Optional, Tuple
+
+LAST: int = -1
+
+Instruction = namedtuple("Instruction", ["source", "destination", "quantity"])
+Stack = Dict[str, List[str]]
 
 
-def solution(filename: str) -> str:
+def parse(filename: str) -> Tuple[Stack, List[Instruction]]:
     with open(filename, "r") as fp:
         raw_data: str = fp.read()
 
-    stacks_data, instructions = raw_data.split("\n\n")
-    stacks_matrix: List[List] = [list(row) for row in stacks_data.splitlines()]
+    stacks_data, instructions_data = raw_data.split("\n\n")
+    stack_lines: List[str] = stacks_data.splitlines()
 
-    stacks: List[List] = []
+    number_of_rows: int = len(stack_lines)
 
-    for row in [*zip(*stacks_matrix)]:
-        if row[-1].isnumeric():
-            stacks.append(list("".join(reversed(list(row))).strip())[1:])
+    stacks: Dict[str, List[str]] = {}
 
-    for instruction in instructions.splitlines():
-        move_data = re.match("move (\d+) from (\d+) to (\d+)", instruction)
-        number_of_crates: int = int(move_data.group(1))
-        source: int = int(move_data.group(2))
-        destination: int = int(move_data.group(3))
+    for match in re.finditer(r"\d", stack_lines[LAST]):
+        matched_text = match.group()
+        start_index = match.start()
+        stacks[matched_text] = [
+            stack_lines[i][start_index]
+            for i in range(number_of_rows - 2, -1, -1)
+            if stack_lines[i][start_index] != " "
+        ]
 
-        # move crates
-        stack_hight: int = len(stacks[source - 1])
-        crates_to_move: List[str] = stacks[source - 1][-number_of_crates:]
-        stacks[source - 1] = stacks[source - 1][: stack_hight - number_of_crates]
-        stacks[destination - 1].extend(crates_to_move)
+    instructions: List[Instruction] = []
 
-    message: List[str] = [row[-1] for row in stacks]
+    for instruction in instructions_data.splitlines():
+        move_data: Optional[Match[str]] = re.match(
+            r"move (\d+) from (\d+) to (\d+)", instruction
+        )
 
+        if move_data is not None:
+
+            number_of_crates: int = int(move_data.group(1))
+            source: str = move_data.group(2)
+            destination: str = move_data.group(3)
+
+            instructions.append(Instruction(source, destination, number_of_crates))
+
+    return stacks, instructions
+
+
+def solve(stacks: Stack, instructions: List[Instruction]) -> str:
+    for instr in instructions:
+        stack_hight: int = len(stacks[instr.source])
+        crates_to_move: List[str] = stacks[instr.source][-instr.quantity :]
+        stacks[instr.source] = stacks[instr.source][: stack_hight - instr.quantity]
+        stacks[instr.destination].extend(crates_to_move)
+
+    message: List[str] = [row[LAST] for row in stacks.values()]
     return "".join(message)
 
 
-if __name__ == "__main__":
-    result: int = solution("./example.txt")
-    print(result)  # it should be 2
+def solution(filename: str) -> str:
+    stacks, instructions = parse(filename)
+    return solve(stacks, instructions)
 
-    result = solution("./input.txt")
-    print(result)
+
+if __name__ == "__main__":
+    print(solution("./example.txt"))  # "MCD"
+    print(solution("./input.txt"))  # "LVZPSTTCZ"

7fc587b1906f3ac57105d3bbadc83cb12fe506b0
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2026-07-20 21:29:39 -0500

    Lint and refactor day 8 in python. Both parts

--- python/day08/part1.py
+++ python/day08/part1.py
@@ -1,81 +1,67 @@
-from typing import List
+from typing import List, Set, Tuple
+
+Trees = List[List[int]]
 
 
-class Tree:
-    def __init__(self, height: int) -> None:
-        self.height = int(height)
-        self.visible = False
+def parse(filename: str) -> Trees:
+    with open(filename, "r") as fp:
+        data: List[str] = fp.read().splitlines()
 
-    def set_visible(self, visibility: bool) -> None:
-        self.visible = visibility
+    return [[int(tree) for tree in row] for row in data]
 
-    def is_visible(self) -> bool:
-        return self.visible
+
+def solve(trees: Trees) -> int:
+    size: int = len(trees)
+    visible_trees: Set[Tuple[int, int]] = set()
+
+    # top row
+    for col in range(size):
+        max_tree: int = trees[0][col]
+        visible_trees.add((0, col))
+
+        for row in range(size):
+            if trees[row][col] > max_tree:
+                visible_trees.add((row, col))
+                max_tree = max(max_tree, trees[row][col])
+
+    # right col
+    for row in range(size):
+        max_tree = trees[row][size - 1]
+        visible_trees.add((row, size - 1))
+
+        for col in range(size - 1, -1, -1):
+            if trees[row][col] > max_tree:
+                visible_trees.add((row, col))
+                max_tree = max(max_tree, trees[row][col])
+
+    # bottom row
+    for col in range(size):
+        max_tree = trees[size - 1][col]
+        visible_trees.add((size - 1, col))
+
+        for row in range(size - 1, -1, -1):
+            if trees[row][col] > max_tree:
+                visible_trees.add((row, col))
+                max_tree = max(max_tree, trees[row][col])
+
+    # left col
+    for row in range(size):
+        max_tree = trees[row][0]
+        visible_trees.add((row, 0))
+
+        for col in range(size):
+            if trees[row][col] > max_tree:
+                visible_trees.add((row, col))
+                max_tree = max(max_tree, trees[row][col])
+
+    return len(visible_trees)
 
 
 def solution(filename: str) -> int:
-    with open(filename, "r") as fp:
-        data: str = fp.read()
-
-    trees: List[List[str]] = [list(map(Tree, row)) for row in data.splitlines()]
-    trees_size: int = len(trees)
-
-    # top row
-    for col in range(trees_size):
-        trees[0][col].set_visible(True)
-        max_tree: int = trees[0][col].height
-
-        for row in range(1, trees_size):
-            current_tree: Tree = trees[row][col]
-            if current_tree.height > max_tree:
-                current_tree.set_visible(True)
-                max_tree = max(max_tree, current_tree.height)
-
-    # right col
-    for row in range(trees_size):
-        trees[row][trees_size - 1].set_visible(True)
-        max_tree: int = trees[row][trees_size - 1].height
-
-        for col in range(trees_size - 2, -1, -1):
-            current_tree: Tree = trees[row][col]
-            if current_tree.height > max_tree:
-                current_tree.set_visible(True)
-                max_tree = max(max_tree, current_tree.height)
-
-    # bottom row
-    for col in range(trees_size):
-        trees[trees_size - 1][col].set_visible(True)
-        max_tree: int = trees[trees_size - 1][col].height
-
-        for row in range(trees_size - 2, -1, -1):
-            current_tree: Tree = trees[row][col]
-            if current_tree.height > max_tree:
-                current_tree.set_visible(True)
-                max_tree = max(max_tree, current_tree.height)
-
-    # left col
-    for row in range(trees_size):
-        trees[row][0].set_visible(True)
-        max_tree: int = trees[row][0].height
-
-        for col in range(1, trees_size - 1):
-            current_tree: Tree = trees[row][col]
-            if current_tree.height > max_tree:
-                current_tree.set_visible(True)
-                max_tree = max(max_tree, current_tree.height)
-
-    # count visible trees:
-    visible_trees: int = 0
-    for row in trees:
-        for tree in row:
-            if tree.is_visible():
-                visible_trees += 1
-    return visible_trees
+    trees: Trees = parse(filename)
+    return solve(trees)
 
 
 if __name__ == "__main__":
-    result: int = solution("./example.txt")
-    print(result)
-
-    result = solution("./input.txt")
-    print(result)
+    print(solution("./example.txt"))  # 21
+    print(solution("./input.txt"))  # 1835

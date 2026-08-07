1c1967872d6cdc3d89b3b667d5ff3af661ce16fd
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2026-08-07 15:46:40 -0500

    Refactor and lint day 12 in python. Both parts

--- python/day12/part2.py
+++ python/day12/part2.py
@@ -1,83 +1,89 @@
-from typing import List, Set, Tuple, Deque
-from collections import deque
+from collections import deque, namedtuple
+from typing import Deque, List, Set, Tuple
 
-START = "E"
-END = "a"
+START: str = "E"
+END: str = "a"
 
 
-def is_at_most_one_higher(neighbor: Tuple, current: Tuple, grid) -> bool:
-    n_row, n_col = neighbor
-    c_row, c_col = current
-    neighbor_char: str = grid[n_row][n_col]
-    current_char: str = grid[c_row][c_col]
+type Grid = List[List[str]]
+Position = namedtuple("Position", ["row", "col"])
+
+
+def parse(filename: str) -> Tuple[Grid, Position, Position]:
+    with open(filename, "r") as fp:
+        raw_data: str = fp.read()
+
+    grid: Grid = []
+    for line in raw_data.splitlines():
+        grid.append([char for char in line])
+
+    # find start and end
+    start: Position
+    end: Position
+    for i, row in enumerate(grid):
+        for j, elevation in enumerate(row):
+            if elevation == START:
+                start = Position(i, j)
+            elif elevation == END:
+                end = Position(i, j)
+
+    # patch start point
+    grid[start.row][start.col] = "z"
+
+    return grid, start, end
+
+
+def is_at_most_one_higher(neighbor: Position, current: Position, grid: Grid) -> bool:
+    neighbor_char: str = grid[neighbor.row][neighbor.col]
+    current_char: str = grid[current.row][current.col]
 
     return (ord(neighbor_char) - ord(current_char)) <= 1
 
 
-def neighbors(
-    position: Tuple[int, int], max_row: int, max_col: int
-) -> List[Tuple[int, int]]:
+def neighbors(position: Position, max_row: int, max_col: int) -> List[Position]:
+    steps: List[Tuple[int, int]] = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-    current_row, current_col = position
-    steps = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-    result: List[Tuple[int, int]] = []
+    result: List[Position] = []
     for step_row, step_col in steps:
         if (
-            0 <= current_row + step_row < max_row
-            and 0 <= current_col + step_col < max_col
+            0 <= position.row + step_row < max_row
+            and 0 <= position.col + step_col < max_col
         ):
-            result.append((current_row + step_row, current_col + step_col))
+            result.append(Position(position.row + step_row, position.col + step_col))
     return result
 
 
-def solution(filename: str) -> int:
-    with open(filename, "r") as fp:
-        raw_data: str = fp.read()
-
-    grid: List[List[str]] = []
-    for line in raw_data.splitlines():
-        grid.append([char for char in line])
-
-    for i, row in enumerate(grid):
-        for j, elevation in enumerate(row):
-            if elevation == START:
-                start: tuple[str, str] = (i, j)
-
-    # patch start point
-    grid[start[0]][start[1]] = "z"
-
-    # BFS for shortest path ----------------------------------------------------
+def solve(grid: Grid, start: Position, end: Position) -> int:
+    # BFS for shortest path
 
     # BFS initialization
     max_rows: int = len(grid)
     max_cols: int = len(grid[0])
-    queue: Deque = deque()
-    queue.append((start, []))
-    visited: Set[Tuple[int, int]] = {start}
+    queue: Deque[Tuple[Position, int]] = deque([(start, 0)])
+    visited: Set[Position] = {start}
 
     # main BFS traverse
     while queue:
-        current, path = queue.popleft()
-        if grid[current[0]][current[1]] == END:
-            final_path = path
-            break
+        current, steps = queue.popleft()
+
+        if grid[current.row][current.col] == END:
+            return steps
+
         for neighbor in neighbors(current, max_rows, max_cols):
             if neighbor not in visited and is_at_most_one_higher(
                 current, neighbor, grid  # diff arguments order for part2
             ):
-                new_path = path[:]
-                new_path.append(neighbor)
-                queue.append((neighbor, new_path))
-
+                queue.append((neighbor, steps + 1))
                 visited.add(neighbor)
 
-    return len(final_path)
+    return -1
+
+
+def solution(filename: str) -> int:
+    grid, start, end = parse(filename)
+    return solve(grid, start, end)
 
 
 if __name__ == "__main__":
-    result: int = solution("./example.txt")
-    print(result)  # it should be 31
-
-    result = solution("./input.txt")
-    print(result)
+    print(solution("./example.txt"))  # 29
+    print(solution("./input.txt"))  # 418

152cc0020b579c2ad440fe40006bfef0230a9e3a
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2026-08-20 14:01:40 -0500

    Lint and refactor day 15 in python. Both parts

--- python/day15/part1.py
+++ python/day15/part1.py
@@ -1,5 +1,10 @@
 import re
-from typing import Dict, Tuple, List, Set
+from collections import namedtuple
+from typing import List, Match, Optional, Set, Tuple
+
+LAST: int = -1
+
+Interval = namedtuple("Interval", ["start", "end"])
 
 
 class Sensor:
@@ -8,81 +13,113 @@ class Sensor:
     ) -> None:
         self.x: int = sensor_x
         self.y: int = sensor_y
+        self.beacon_x: int = beacon_x
+        self.beacon_y: int = beacon_y
         self.manhattan_radius: int = abs(beacon_x - sensor_x) + abs(sensor_y - beacon_y)
         self.lower_reach: int = sensor_y - self.manhattan_radius
         self.high_reach: int = sensor_y + self.manhattan_radius
 
+    def __repr__(self) -> str:
+        return f"({self.x}, {self.y}) radius: {self.manhattan_radius} reach: {self.lower_reach}, {self.high_reach}"
 
-def merge(intervals: List[List[int]]) -> List[List[int]]:
-    intervals.sort(key=lambda x: x[0])
 
-    merged: List[List[int]] = [intervals[0]]
+class Beacon:
+    def __init__(self, beacon_x: int, beacon_y: int) -> None:
+        self.x: int = beacon_x
+        self.y: int = beacon_y
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Beacon):
+            return False
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+
+    def __repr__(self) -> str:
+        return f"({self.x}, {self.y})"
+
+
+def merge(intervals: List[Interval]) -> List[Interval]:
+    intervals.sort(key=lambda x: x.start)
+    merged: List[Interval] = [intervals[0]]  # 1 <= intervals.length <= 10^
+
     for i in range(1, len(intervals)):
-        if intervals[i][0] <= merged[-1][1]:
-            merged[-1][0] = min(merged[-1][0], intervals[i][0])
-            merged[-1][1] = max(merged[-1][1], intervals[i][1])
+        if intervals[i].start <= merged[-1].end:
+            merged[LAST] = Interval(
+                min(merged[LAST].start, intervals[i].start),
+                max(merged[LAST].end, intervals[i].end),
+            )
         else:
             merged.append(intervals[i])
+
     return merged
 
 
-def parse(filename: int) -> Tuple[Dict, Dict]:
+def parse(filename: str) -> Tuple[List[Sensor], Set[Beacon]]:
     with open(filename, "r") as fp:
-        data: str = fp.read().splitlines()
+        data: List[str] = fp.read().splitlines()
 
-    expr = r"Sensor at x=(-*\d+), y=(-*\d+): closest beacon is at x=(-*\d+), y=(-*\d+)"
+    expr: str = (
+        r"Sensor at x=(-*\d+), y=(-*\d+): closest beacon is at x=(-*\d+), y=(-*\d+)"
+    )
 
-    # parse input and create sensors and beacons dictionaries
-    sensors: Dict[Tuple[int, int], Sensor] = {}
-    beacon_in_row: Dict[int, Set[int]] = {}
+    sensors: List[Sensor] = []
+    beacons: Set[Beacon] = set()
+
     for line in data:
-        result = re.match(expr, line)
-        sensor_x = int(result.group(1))
-        sensor_y = int(result.group(2))
-        beacon_x = int(result.group(3))
-        beacon_y = int(result.group(4))
+        result: Optional[Match[str]] = re.match(expr, line)
+        if result:
+            sensor_x: int = int(result.group(1))
+            sensor_y: int = int(result.group(2))
+            beacon_x: int = int(result.group(3))
+            beacon_y: int = int(result.group(4))
 
-        sensors[(sensor_x, sensor_y)] = Sensor(sensor_x, sensor_y, beacon_x, beacon_y)
-        if beacon_y in beacon_in_row:
-            beacon_in_row[beacon_y].add(beacon_x)
-        else:
-            beacon_in_row[beacon_y] = set([beacon_x])
+        sensors.append(Sensor(sensor_x, sensor_y, beacon_x, beacon_y))
+        beacons.add(Beacon(beacon_x, beacon_y))
 
-    return sensors, beacon_in_row
+    return sensors, beacons
 
 
-def solution(filename: str, row: int) -> int:
-    sensors, beacon_in_row = parse(filename)
+def solve(
+    sensors: List[Sensor],
+    beacons: Set[Beacon],
+    row: int,
+) -> int:
 
     # count intersections
-    intersection_intervals: List[List[int]] = []
-    for sensor in sensors.values():
+    intersections: List[Interval] = []
+
+    for sensor in sensors:
         if sensor.lower_reach <= row <= sensor.high_reach:
             distance_from_sensor: int = abs(sensor.y - row)
             reminder_distance: int = sensor.manhattan_radius - distance_from_sensor
 
-            intersection: List[int] = [
+            intersection: Interval = Interval(
                 sensor.x - reminder_distance,
                 sensor.x + reminder_distance,
-            ]
-            intersection_intervals.append(intersection)
+            )
+            intersections.append(intersection)
 
-    # merge all intervals
-    merged_intervals: List[List[int]] = merge(intersection_intervals)
+    merged_intervals: List[Interval] = merge(intersections)
 
-    # calculate all intersection points
     total_intersections: int = sum(
         [(interval[1] - interval[0] + 1) for interval in merged_intervals]
     )
-
-    # substract beacons that are on the row `row`
-    total_intersections -= len(beacon_in_row.get(row, dict()))
+    # subtract beacons on the row `row`
+    for beacon in beacons:
+        if beacon.y == row:
+            for start, end in merged_intervals:
+                if start <= beacon.x <= end:
+                    total_intersections -= 1
     return total_intersections
 
 
-if __name__ == "__main__":
-    result: int = solution("./example.txt", 10)
-    print(result)  # it should be 26
+def solution(filename: str, row: int) -> int:
+    sensors, beacons = parse(filename)
+    return solve(sensors, beacons, row)
 
-    result = solution("./input.txt", 2_000_000)
-    print(result)
+
+if __name__ == "__main__":
+    print(solution("./example.txt", 10))  # 26
+    print(solution("./input.txt", 2_000_000))  # 4724228
--- python/day15/part2.py
+++ python/day15/part2.py
@@ -1,56 +1,97 @@
 import re
-from typing import Dict, Tuple, List
+from typing import List, Match, Optional, Tuple
 
-from part1 import Sensor, parse
+MULTIPLIER: int = 4_000_000
 
 
-def merge(intervals: List[List[int]]) -> List[List[int]]:
-    intervals.sort(key=lambda x: x[0])
+class Sensor:
+    def __init__(
+        self, sensor_x: int, sensor_y: int, beacon_x: int, beacon_y: int
+    ) -> None:
+        self.x: int = sensor_x
+        self.y: int = sensor_y
+        self.manhattan_radius: int = abs(beacon_x - sensor_x) + abs(sensor_y - beacon_y)
 
-    merged: List[List[int]] = [intervals[0]]
-    for i in range(1, len(intervals)):
-        if intervals[i][0] <= merged[-1][1] or merged[-1][-1] + 1 == intervals[i][0]:
-            merged[-1][0] = min(merged[-1][0], intervals[i][0])
-            merged[-1][1] = max(merged[-1][1], intervals[i][1])
+    def __repr__(self) -> str:
+        return f"({self.x}, {self.y}) radius: {self.manhattan_radius}"
+
+
+def parse(filename: str) -> List[Sensor]:
+    with open(filename, "r") as fp:
+        data: List[str] = fp.read().splitlines()
+
+    expr: str = (
+        r"Sensor at x=(-*\d+), y=(-*\d+): closest beacon is at x=(-*\d+), y=(-*\d+)"
+    )
+
+    sensors: List[Sensor] = []
+    for line in data:
+        result: Optional[Match[str]] = re.match(expr, line)
+        if result:
+            sensor_x: int = int(result.group(1))
+            sensor_y: int = int(result.group(2))
+            beacon_x: int = int(result.group(3))
+            beacon_y: int = int(result.group(4))
+        sensors.append(Sensor(sensor_x, sensor_y, beacon_x, beacon_y))
+
+    return sensors
+
+
+def solve(sensors: List[Sensor], max_dimension: int) -> int:
+    n: int = len(sensors)
+    adjacent: List[Tuple[Sensor, Sensor]] = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            s1: Sensor = sensors[i]
+            s2: Sensor = sensors[j]
+            distance: int = abs(s1.x - s2.x) + abs(s1.y - s2.y)
+            if distance == (s1.manhattan_radius + s2.manhattan_radius + 2):
+                adjacent.append((s1, s2))
+
+    # input has only 2 pairs, however example has multiple matching pairs
+    adjacent = adjacent[:2]
+
+    # determine coordinates of the diagonals
+    left: Sensor
+    right: Sensor
+    sign: int
+    x: int
+    y: int
+    coords: List[Tuple[int, int, int]] = []
+
+    for s1, s2 in adjacent:
+        if s1.x < s2.x:
+            left = s1
+            right = s2
         else:
-            merged.append(intervals[i])
-    return merged
+            left = s2
+            right = s1
+
+        if left.y > right.y:
+            x, y = left.x, left.y - left.manhattan_radius - 1
+            sign = -1
+        else:
+            x, y = left.x, left.y + left.manhattan_radius + 1
+            sign = 1
+
+        coords.append((x, y, sign))
+
+    (x1, y1, sign1), (x2, y2, sign2) = coords
+
+    # solve intersections of diagonals
+    x = (x1 + sign1 * y1 + x2 + sign2 * y2) // 2
+    y = -(x1 + sign1 * y1 - x2 - sign2 * y2) // 2
+
+    assert 0 <= x <= max_dimension and 0 <= y <= max_dimension
+
+    return (MULTIPLIER * x) + y
 
 
-def solution(sensors: Dict, beacon_in_row: Dict, row: int) -> int:
-    # count intersections
-    intersection_intervals: List[List[int]] = []
-    for sensor in sensors.values():
-        if sensor.lower_reach <= row <= sensor.high_reach:
-            reminder_distance: int = sensor.manhattan_radius - abs(sensor.y - row)
-
-            intersection: List[int] = [
-                sensor.x - reminder_distance,
-                sensor.x + reminder_distance,
-            ]
-            intersection_intervals.append(intersection)
-
-    # merge all intervals
-    merged_intervals: List[List[int]] = merge(intersection_intervals)
-
-    if len(merged_intervals) == 1:
-        return None
-
-    return 4_000_000 * (merged_intervals[0][-1] + 1) + row
-
-
-def solve(filename: int, max_row: int) -> int:
-    sensors, beacon_in_row = parse(filename)
-
-    for row in range(max_row + 1):
-        result = solution(sensors, beacon_in_row, row)
-        if result is not None:
-            return result
+def solution(filename: str, row: int) -> int:
+    sensors: List[Sensor] = parse(filename)
+    return solve(sensors, row)
 
 
 if __name__ == "__main__":
-    result: int = solve("./example.txt", 20)
-    print(result)  # it should be 56000011
-
-    result = solve("./input.txt", 4000000)
-    print(result)
+    print(solution("./example.txt", 20))  # 56000011
+    print(solution("./input.txt", 4_000_000))  # 13622251246513
